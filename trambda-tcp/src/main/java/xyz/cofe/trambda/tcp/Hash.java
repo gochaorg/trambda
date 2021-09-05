cd25740acdabf5164268265751102f7c8d35b54b
Kamnev Georgiy <nt.gocha@gmail.com>
2021-09-06 04:03:02 +0500

    java doc

--- trambda-tcp/src/main/java/xyz/cofe/trambda/tcp/Hash.java
+++ trambda-tcp/src/main/java/xyz/cofe/trambda/tcp/Hash.java
@@ -3,7 +3,18 @@ package xyz.cofe.trambda.tcp;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+/**
+ * Класс утилита для вычисления хеша
+ */
 public class Hash {
+
+    /**
+     * Вычисление MD5
+     * @param data данные
+     * @param off смещение
+     * @param size размер
+     * @return результат
+     */
     public static byte[] md5(byte[] data,int off,int size){
         if( data==null )throw new IllegalArgumentException( "data==null" );
         if( off<0 )throw new IllegalArgumentException( "off<0" );
